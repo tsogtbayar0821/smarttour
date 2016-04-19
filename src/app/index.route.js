1c50c8d29b1f8ca7bf660a119de7b9d0fc1cfaeb
Tsogtbayar Davaadorj <tsogtbayar0821@hotmail.com>
2016-04-19 15:22:43 -0400

    bootstrap-sass deleted

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,21 +1,65 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('tour')
-    .config(routerConfig);
+    angular
+        .module('tour')
+        .config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
+    /** @ngInject */
+    function routerConfig($stateProvider, $urlRouterProvider, TEMPLATES_DIR) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/templates/home/main.html',
+                controller: 'MainController',
+                controllerAs: 'main'
+            });
+        // --- Hotel ---
+        var hotelTemplatesDir = TEMPLATES_DIR + 'hotel/';
+        $stateProvider
+            .state('hotel', {
+                url: '/hotel',
+                template: '<ui-view />',
+            });
 
-    $urlRouterProvider.otherwise('/');
-  }
+        // Sentences
+        $stateProvider
+            .state('hotel.sentences', {
+                url: '/sentences',
+                template: '<ui-view />',
+            })
+            .state('hotel.sentences.create', {
+                url: '/create?closeOnDone&forcedTypeId',
+                controller: 'SentenceEditController',
+                controllerAs: 'vm',
+                templateUrl: hotelTemplatesDir + 'sentence_form.html',
+                resolve: {
+                    sentenceInstance: function(Sentence) {
+                        return new Sentence({ id: null, type_id: null });
+                    },
+                    localLanguage: function($translate) {
+                        return $translate.use();
+                    },
+                },
+            })
+            .state('hotel.sentences.edit', {
+                url: '/edit/:id?closeOnDone',
+                controller: 'SentenceEditController',
+                controllerAs: 'vm',
+                templateUrl: hotelTemplatesDir + 'sentence_form.html',
+                resolve: {
+                    sentenceInstance: function($stateParams, Sentence) {
+                        return Sentence.get({ id: $stateParams.id });
+                    },
+                    localLanguage: function($translate) {
+                        return $translate.use();
+                    },
+                },
+            });
+
+
+
+        $urlRouterProvider.otherwise('/');
+    }
 
 })();
