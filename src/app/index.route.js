b9d2be2444393e49e35fea1b9ef20554e61d2995
Tsogtbayar Davaadorj <tsogtbayar0821@hotmail.com>
2016-04-19 23:26:11 -0400

    routes added v2

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -34,7 +34,125 @@
                 controllerAs: 'vm',
                 templateUrl: hotelTemplatesDir + 'hotel_detail.html',
             });
-
+        $stateProvider
+            .state('booking', {
+                url: '/booking',
+                template: '<ui-view />',
+            })
+            .state('booking.list', {
+                url: '/list',
+                controller: 'BookingListController',
+                controllerAs: 'booking',
+                templateUrl: bookingTemplatesDir + 'booking_list.html',
+            })
+            .state('booking.detail', {
+                url: '/detail/:id',
+                controller: 'BookingDetailController',
+                controllerAs: 'vm',
+                templateUrl: bookingTemplatesDir + 'booking_detail.html',
+            });
+        $stateProvider
+            .state('campsite', {
+                url: '/campsite',
+                template: '<ui-view />',
+            })
+            .state('campsite.list', {
+                url: '/list',
+                controller: 'CampsiteListController',
+                controllerAs: 'campsite',
+                templateUrl: campsiteTemplatesDir + 'campsite_list.html',
+            })
+            .state('campsite.detail', {
+                url: '/detail/:id',
+                controller: 'CampsiteDetailController',
+                controllerAs: 'vm',
+                templateUrl: campsiteTemplatesDir + 'campsite_detail.html',
+            });
+        $stateProvider
+            .state('events', {
+                url: '/events',
+                template: '<ui-view />',
+            })
+            .state('events.list', {
+                url: '/list',
+                controller: 'EventListController',
+                controllerAs: 'events',
+                templateUrl: eventsTemplatesDir + 'events_list.html',
+            })
+            .state('events.detail', {
+                url: '/detail/:id',
+                controller: 'EventDetailController',
+                controllerAs: 'vm',
+                templateUrl: eventsTemplatesDir + 'events_detail.html',
+            });
+        $stateProvider
+            .state('recent_search', {
+                url: '/recent_search',
+                template: '<ui-view />',
+            })
+            .state('recent_search.list', {
+                url: '/list',
+                controller: 'RecentSearchListController',
+                controllerAs: 'recent_search',
+                templateUrl: recent_searchTemplatesDir + 'recent_search_list.html',
+            })
+            .state('recent_search.detail', {
+                url: '/detail/:id',
+                controller: 'RecentSearchDetailController',
+                controllerAs: 'vm',
+                templateUrl: recent_searchTemplatesDir + 'recent_search_detail.html',
+            });
+        $stateProvider
+            .state('restaurant', {
+                url: '/restaurant',
+                template: '<ui-view />',
+            })
+            .state('restaurant.list', {
+                url: '/list',
+                controller: 'RestaurantListController',
+                controllerAs: 'restaurant',
+                templateUrl: restaurantTemplatesDir + 'restaurant_list.html',
+            })
+            .state('restaurant.detail', {
+                url: '/detail/:id',
+                controller: 'RestaurantDetailController',
+                controllerAs: 'vm',
+                templateUrl: restaurantTemplatesDir + 'restaurant_detail.html',
+            });
+        $stateProvider
+            .state('review', {
+                url: '/review',
+                template: '<ui-view />',
+            })
+            .state('review.list', {
+                url: '/list',
+                controller: 'ReviewListController',
+                controllerAs: 'review',
+                templateUrl: reviewTemplatesDir + 'review_list.html',
+            })
+            .state('review.detail', {
+                url: '/detail/:id',
+                controller: 'ReviewDetailController',
+                controllerAs: 'vm',
+                templateUrl: reviewTemplatesDir + 'review_detail.html',
+            });
+        $stateProvider
+            .state('tour', {
+                url: '/tour',
+                template: '<ui-view />',
+            })
+            .state('tour.list', {
+                url: '/list',
+                controller: 'TourListController',
+                controllerAs: 'tour',
+                templateUrl: tourTemplatesDir + 'tour_list.html',
+            })
+            .state('tour.detail', {
+                url: '/detail/:id',
+                controller: 'TourDetailController',
+                controllerAs: 'vm',
+                templateUrl: tourTemplatesDir + 'tour_detail.html',
+            });
 
 
         $urlRouterProvider.otherwise('/');
